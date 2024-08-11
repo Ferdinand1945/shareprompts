ce1956d8dba7f187c7eeb910972efa56827485ef
Fernando Lopez <flopez@iolabs.se>
2024-08-11 19:46:43 +0200

    fixes for p tags and paddings

--- app/page.jsx
+++ app/page.jsx
@@ -7,8 +7,8 @@ const Home = () => {
     <br className="max-md:hidden" />
     <span className="orange_gradient text-center">AI-Powered promt</span>
     </h1>
-    <p>
-        PromtAI is an open source project to discover and share ai prompts. Search by tag or content and visit the prompts creator profile to check their work!
+    <p className="mt-5 text-lg text-center">
+        PromtAI is an open source project to discover and share ai prompts. <br /> Search by tag or content and visit the prompts creator profile to check their work!
     </p>
 
     <Feed />
